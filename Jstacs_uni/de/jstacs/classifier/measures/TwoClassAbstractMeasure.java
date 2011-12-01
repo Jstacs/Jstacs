fc47a1313dbce8bd5e214e76f7e72d62edbea5cc
keilwagen <keilwagen@feecfdc5-8703-4d9e-922f-b95afb99c251>
2011-12-01 07:58:26 +0000

    modification of AbstractMeasure and MeasureParameters

--- Jstacs_uni/de/jstacs/classifier/measures/TwoClassAbstractMeasure.java
+++ Jstacs_uni/de/jstacs/classifier/measures/TwoClassAbstractMeasure.java
@@ -22,13 +22,13 @@ public abstract class TwoClassAbstractMeasure extends AbstractMeasure {
 
 	@Override
 	public final ResultSet compute( double[][][] classSpecificScores ) {
-		double[][] classificationScores = new double[classSpecificScores.length][];
+		if(classSpecificScores.length != 2){
+			throw new RuntimeException( "Only two classes possible for "+getClass().getSimpleName() );//XXX getName()
+		}
+		double[][] classificationScores = new double[2][];
 		for(int i=0;i<classSpecificScores.length;i++){
 			classificationScores[i] = new double[classSpecificScores[i].length];
 			for(int j=0;j<classSpecificScores[i].length;j++){
-				if(classSpecificScores[i][j].length != 2){
-					throw new RuntimeException( "Only two classes possible for "+getClass().getSimpleName() );
-				}
 				classificationScores[i][j] = classSpecificScores[i][j][0] - classSpecificScores[i][j][1];
 			}
 			Arrays.sort( classificationScores[i] );
@@ -40,5 +40,4 @@ public abstract class TwoClassAbstractMeasure extends AbstractMeasure {
 	public final int getAllowedNumberOfClasses() {
 		return 2;
 	}
-
 }
