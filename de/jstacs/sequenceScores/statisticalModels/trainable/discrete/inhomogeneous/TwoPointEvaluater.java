a164428abd73951e6684822ab2d8c24df628dc6d
keili <jens.keilwagen@googlemail.com>
2012-10-23 10:39:02 +0000

    bugfix (MI in Bits) and new feature (create picture with MI and (corrected) p-values)

--- de/jstacs/sequenceScores/statisticalModels/trainable/discrete/inhomogeneous/TwoPointEvaluater.java
+++ de/jstacs/sequenceScores/statisticalModels/trainable/discrete/inhomogeneous/TwoPointEvaluater.java
@@ -19,6 +19,7 @@
 package de.jstacs.sequenceScores.statisticalModels.trainable.discrete.inhomogeneous;
 
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 import de.jstacs.algorithms.graphs.tensor.SymmetricTensor;
 import de.jstacs.data.DataSet;
@@ -37,7 +38,7 @@ public class TwoPointEvaluater {
 	private static final byte order = 1;
 
 	/**
-	 * This method computes the pairwise mutual information (in bits) between
+	 * This method computes the pairwise mutual information between
 	 * the sequence positions.
 	 * 
 	 * @param s
@@ -46,7 +47,7 @@ public class TwoPointEvaluater {
 	 *            the weights for each sequence of the {@link DataSet} or
 	 *            <code>null</code>
 	 * 
-	 * @return a matrix containing all pairwise mutual informations (in bits)
+	 * @return a matrix containing all pairwise mutual informations
 	 * 
 	 * @throws IllegalArgumentException
 	 *             if something went wrong (e.g. the
@@ -54,7 +55,7 @@ public class TwoPointEvaluater {
 	 *             length is not matching with the
 	 *             {@link de.jstacs.data.AlphabetContainer}, ...)
 	 */
-	public static double[][] getMIInBits( DataSet s, double[] weights ) throws IllegalArgumentException {
+	public static double[][] getMI( DataSet s, double[] weights ) throws IllegalArgumentException {
 		int l = s.getElementLength(), i = 0;
 		int[] j = new int[1];
 		StructureLearner sl = new StructureLearner( s.getAlphabetContainer(), l );
@@ -78,6 +79,34 @@ public class TwoPointEvaluater {
 			for( j[0] = i + 1; j[0] < l; j[0]++ ) {
 				mi[i][j[0]] = mi[j[0]][i] = tensor.getValue( order, i, j ) / sum;
 				//System.out.println( mi[i][j[0]] );
+			}
+		}
+		return mi;
+	}
+	
+	/**
+	 * This method computes the pairwise mutual information (in bits) between
+	 * the sequence positions.
+	 * 
+	 * @param s
+	 *            the {@link DataSet}
+	 * @param weights
+	 *            the weights for each sequence of the {@link DataSet} or
+	 *            <code>null</code>
+	 * 
+	 * @return a matrix containing all pairwise mutual informations (in bits)
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if something went wrong (e.g. the
+	 *             {@link de.jstacs.data.AlphabetContainer} is not discrete, the
+	 *             length is not matching with the
+	 *             {@link de.jstacs.data.AlphabetContainer}, ...)
+	 */
+	public static double[][] getMIInBits( DataSet s, double[] weights ) throws IllegalArgumentException {
+		double[][] mi = getMI(s, weights);
+		for( int i = 0; i < mi.length; i++ ) {
+			for( int j = 0; j < mi[i].length; j++ ) {
+				mi[i][j] = mi[i][j]  / Math.log(2);
 			}
 		}
 		return mi;
@@ -110,6 +139,73 @@ public class TwoPointEvaluater {
 									+ "axis( 4, seq(mini,maxi,length=5), round( seq(mini,maxi,length=5), digits = 4 ) )",
 				1080,
 				864 );
+		//r.eval( "layout( matrix( 1 ), height=1, width=1 );" );
+		return b;
+	}
+	
+	public static BufferedImage getImage(DataSet d, double[] weights, REnvironment r, double alpha, int... borders ) throws Exception {
+		double[][] mi = getMI(d, weights);//TODO
+		r.createMatrix( "mi", mi );
+		r.createVector( "borders", borders );
+		r.eval("L=dim(mi)[1];l=1:L;");
+		double sum = 0;
+		if( weights != null ) {
+			for( int i = 0; i < weights.length; i++ ) {
+				sum += weights[i];
+			}
+		} else {
+			sum = d.getNumberOfElements();
+		}
+		r.eval("f=2*"+sum);
+		int[] a = new int[d.getElementLength()];
+		for( int i = 0; i < a.length; i++ ) {
+			a[i] = (int) d.getAlphabetContainer().getAlphabetLengthAt(i);
+		}
+		r.createVector("a", a);
+		r.voidEval( "p=matrix(NA,ncol=L,nrow=L);" );
+		r.voidEval( "count=rep(0,L);" );
+		r.voidEval( "correction=(L*(L-1)/2); alpha = " + alpha + ";" );
+		r.voidEval( "for( i in l ) { for( j in i:L ) { "
+					+ "mi[j,i]=NA;"
+					+ "if( i != j ) { " 
+						+ "p[j,i]=pchisq(f*mi[i,j],df=(a[i]-1)*(a[j]-1),lower.tail=F)*correction;"
+						+ "if( p[j,i] > alpha ) {"
+							+ "p[j,i]=NA;"
+						+ "} else {"
+							+ "count[i] = count[i] + 1;"
+							+ "count[j] = count[j] + 1;"
+						+ "}\n"
+					+"}\n"
+					+ "}\n }\n" );
+		r.voidEval( "count[count==0]=NA;" );
+		r.eval( "mini = min( mi, na.rm=T ); maxi = max( mi, na.rm=T ); lim = c(mini, maxi);c = rev( gray((1:1000)/1000) ); skala = seq( mini, maxi, length = length(c) ); m = matrix( skala, ncol = length(c) );" );
+		r.eval( "miniP = min( p, na.rm=T ); maxiP = max( p, na.rm=T ); limP = c(miniP, maxiP);colP = rev( gray((800:1)/1000) ); skalaP = seq( miniP, maxiP, length = length(colP) ); mP = matrix( skalaP, ncol = length(colP) );" );
+		//r.eval("seq=\"" + d.getElementAt(0) +"\"; s=rep(NA,L); for( i in l ) { s[i] = substr(seq,i,i); }\n" );
+		BufferedImage b = r.plot(
+				"layout( matrix( c(3,1,4,5,2,6), ncol=3, nrow=2, byrow=T ), width=c(2,11,2), height=c(4,1) ); par( las=1 );" 
+									+ "image( l, l, mi, xlab = \"position\",  ylab = \"position\", zlim = lim, col = c, main = \"\" );"
+									+ "image( l, l, p, add=T, col=colP );"
+									+ "abline(h=borders+0.5,col=2,lwd=3);"
+									+ "abline(v=borders+0.5,col=2,lwd=3);"
+									
+									+ "par( mar = c(5, 4, 0, 2) + 0.1);"
+									+ "plot(l,count,type=\"h\",xlim=c(1,L),lwd=10,xlab=\"position\",ylab=\"# significant MI\");"
+									+ "abline(v=borders+0.5,col=2,lwd=3);"
+									//+ "axis( 3, l, s );"
+									
+									+ "par( mar = c(5, 6, 4, 0) + 0.1);"
+									+ "image( 1, skala, m, col = c, xlab = \"\", ylab = \"\", main = \"MI\", axes = FALSE );"
+									+ "axis( 2, seq(mini,maxi,length=5), round( seq(mini,maxi,length=5), digits = 4 ) );"
+								
+									+ "par( mar = c(5, 0, 4, 6) + 0.1);"
+									+ "image( 1, skalaP, mP, col = colP, xlab = \"\", ylab = \"\", main = \"p-value\", axes = FALSE );"
+									+ "axis( 4, seq(miniP,maxiP,length=5), signif( seq(miniP,maxiP,length=5), 2 ) );"
+									+ "mtext(\"Bonferonie\", 1, 1);"
+									+ "mtext(\"corrected\", 1, 2.5);"
+									,
+
+				1080,
+				1080 );
 		//r.eval( "layout( matrix( 1 ), height=1, width=1 );" );
 		return b;
 	}
