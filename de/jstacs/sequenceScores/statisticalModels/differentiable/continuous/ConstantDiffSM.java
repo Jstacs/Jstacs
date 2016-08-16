d8de22a2dbfc86353c036a97e904dd9d8266d036
keili <jens.keilwagen@googlemail.com>
2016-08-16 13:00:05 +0000

    allowing arbitrary AlphabetContainers implementing fromXML and toXML

--- de/jstacs/sequenceScores/statisticalModels/differentiable/continuous/ConstantDiffSM.java
+++ de/jstacs/sequenceScores/statisticalModels/differentiable/continuous/ConstantDiffSM.java
@@ -7,14 +7,25 @@ import de.jstacs.data.DataSet;
 import de.jstacs.data.alphabets.ContinuousAlphabet;
 import de.jstacs.data.sequences.Sequence;
 import de.jstacs.io.NonParsableException;
+import de.jstacs.io.XMLParser;
 import de.jstacs.sequenceScores.statisticalModels.differentiable.AbstractDifferentiableStatisticalModel;
 import de.jstacs.utils.DoubleList;
 import de.jstacs.utils.IntList;
 
+/**
+ * Simple class for uninformative distribution for all {@link AlphabetContainer}.
+ * It returns the same value (of 0 (zero)) for all possible sequences.
+ * 
+ * @author Jan Grau, Jens Keilwagen
+ */
 public class ConstantDiffSM extends AbstractDifferentiableStatisticalModel {
 
-	public ConstantDiffSM( AlphabetContainer con, int length) throws IllegalArgumentException {
+	public ConstantDiffSM( AlphabetContainer con, int length ) throws IllegalArgumentException {
 		super(con, length);
+	}
+	
+	public ConstantDiffSM( AlphabetContainer con ) throws IllegalArgumentException {
+		this(con, con.getPossibleLength());
 	}
 	
 	public ConstantDiffSM(int length) throws IllegalArgumentException {
@@ -32,6 +43,7 @@ public class ConstantDiffSM extends AbstractDifferentiableStatisticalModel {
 
 	@Override
 	public double getLogNormalizationConstant() {
+		//TODO
 		return 0;
 	}
 
@@ -46,9 +58,7 @@ public class ConstantDiffSM extends AbstractDifferentiableStatisticalModel {
 	}
 
 	@Override
-	public void addGradientOfLogPriorTerm(double[] grad, int start) throws Exception {
-
-	}
+	public void addGradientOfLogPriorTerm(double[] grad, int start) throws Exception {}
 
 	@Override
 	public double getESS() {
@@ -56,14 +66,10 @@ public class ConstantDiffSM extends AbstractDifferentiableStatisticalModel {
 	}
 
 	@Override
-	public void initializeFunction(int index, boolean freeParams, DataSet[] data, double[][] weights) throws Exception {
-
-	}
+	public void initializeFunction(int index, boolean freeParams, DataSet[] data, double[][] weights) throws Exception {}
 
 	@Override
-	public void initializeFunctionRandomly(boolean freeParams) throws Exception {
-
-	}
+	public void initializeFunctionRandomly(boolean freeParams) throws Exception {}
 
 	@Override
 	public double getLogScoreAndPartialDerivation(Sequence seq, int start, IntList indices, DoubleList partialDer) {
@@ -81,9 +87,7 @@ public class ConstantDiffSM extends AbstractDifferentiableStatisticalModel {
 	}
 
 	@Override
-	public void setParameters(double[] params, int start) {
-
-	}
+	public void setParameters(double[] params, int start) {}
 
 	@Override
 	public String getInstanceName() {
@@ -107,14 +111,17 @@ public class ConstantDiffSM extends AbstractDifferentiableStatisticalModel {
 
 	@Override
 	public StringBuffer toXML() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuffer xml = new StringBuffer();
+		XMLParser.appendObjectWithTags(xml, alphabets, "abc");
+		XMLParser.appendObjectWithTags(xml, length, "length");
+		XMLParser.addTags(xml, "ConstantDiffSM");
+		return xml;
 	}
 
 	@Override
 	protected void fromXML(StringBuffer xml) throws NonParsableException {
-		// TODO Auto-generated method stub
-
+		xml = XMLParser.extractForTag(xml, "ConstantDiffSM");
+		alphabets = (AlphabetContainer) XMLParser.extractObjectForTags(xml, "abc");
+		length = (Integer) XMLParser.extractObjectForTags(xml, "length");
 	}
-
 }
