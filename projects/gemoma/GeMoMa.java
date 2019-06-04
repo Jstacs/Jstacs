b93f2711a22912e43aef2cc3540f9c77a34bb7d4
Jens Keilwagen <Jens.Keilwagen@googlemail.com>
2019-06-04 09:51:33 +0200

    new version tag

--- projects/gemoma/GeMoMa.java
+++ projects/gemoma/GeMoMa.java
@@ -97,7 +97,7 @@ import projects.gemoma.Tools.Ambiguity;
  */
 public class GeMoMa implements JstacsTool {
 
-	public static final String VERSION = "1.6.1beta";
+	public static final String VERSION = "1.6.1";
 	
 	public static final String INFO = "#SOFTWARE INFO: ";
 	
