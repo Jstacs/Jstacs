c98d8b61c4339a016480dd6437a826bec108f30b
Jens Keilwagen <Jens.Keilwagen@googlemail.com>
2021-03-22 17:59:14 +0100

    discard genes with no coding transcript

--- projects/gemoma/AnnotationEvidence.java
+++ projects/gemoma/AnnotationEvidence.java
@@ -114,218 +114,230 @@ public class AnnotationEvidence extends GeMoMaModule {
 				}
 				for( int a = 0; a < array.length; a++ ) {
 					Gene g = array[a];
-					g.sortExons();
 					
-					int[][] cov = (coverage && GeMoMa.coverage[g.strand==1?0:1]!= null) ? GeMoMa.coverage[g.strand==1?0:1].get(c) : null;
-					
-					Iterator<Entry<String,Transcript>> cds = g.transcript.entrySet().iterator();
-					
-					annot.append( c + "\t" + g.evidence + "\tgene\t" + g.start + "\t" + g.end + "\t.\t" + (g.strand==1?"+":"-") + "\t.\tID=" + g.id );
-					annot.newLine();
-					
-					while( cds.hasNext() ) {
-						Entry<String,Transcript> e = cds.next();
-						Transcript t = e.getValue();
-						IntList parts = t.b;
-						if( parts.length()>0 ) {
-							int tStart = g.strand>0 ? g.exon.get(parts.get(0))[1] : g.exon.get(parts.get(parts.length()-1))[1];
-							int tEnd = g.strand>0 ? g.exon.get(parts.get(parts.length()-1))[2] : g.exon.get(parts.get(0))[2];
-							if( t.add != null ) {
-								for( int j = 0; j<t.add.size(); j++ ) {
-									String[] s = t.add.get(j);
-									tStart = Math.min(tStart, Integer.parseInt(s[3]) );
-									tEnd = Math.max(tEnd, Integer.parseInt(s[4]));
-								}
-							}
-							
-							w.append( g.id + "\t" + c + "\t" + tStart + "\t" + tEnd + "\t"+ g.strand + "\t" + e.getKey() + "\t" + parts.length() + "\t" );
-							
-							double tie=0;
-							int last=-10;
-							int covered=0, l=0, idx;
-							int[][] donSites = sites==null? null : sites[g.strand==1?0:1];
-							nuc.delete( 0, nuc.length() );
-							double sum=0;
-							int min=Integer.MAX_VALUE;
-							for( int j = 0;j < parts.length(); j++ ) {
-								int[] part = g.exon.get(parts.get(j));
-								
-								if( g.strand> 0 ) {
-									nuc.append( seq.substring( part[1]-1, part[2] ) );
-								} else {
-									nuc.insert(0, seq.substring( part[1]-1, part[2] ) );
+					boolean hasTranscript = false;
+					Iterator<Transcript> ti = g.transcript.values().iterator();
+					while( ti.hasNext() ) {
+						if( ti.next().b.length()>0 ) {
+							hasTranscript=true;
+							break;
+						}
+					}
+
+					if( hasTranscript ) {
+						g.sortExons();
+						
+						int[][] cov = (coverage && GeMoMa.coverage[g.strand==1?0:1]!= null) ? GeMoMa.coverage[g.strand==1?0:1].get(c) : null;
+						
+						Iterator<Entry<String,Transcript>> cds = g.transcript.entrySet().iterator();
+						
+						annot.append( c + "\t" + g.evidence + "\tgene\t" + g.start + "\t" + g.end + "\t.\t" + (g.strand==1?"+":"-") + "\t.\tID=" + g.id );
+						annot.newLine();
+						
+						while( cds.hasNext() ) {
+							Entry<String,Transcript> e = cds.next();
+							Transcript t = e.getValue();
+							IntList parts = t.b;
+							if( parts.length()>0 ) {
+								int tStart = g.strand>0 ? g.exon.get(parts.get(0))[1] : g.exon.get(parts.get(parts.length()-1))[1];
+								int tEnd = g.strand>0 ? g.exon.get(parts.get(parts.length()-1))[2] : g.exon.get(parts.get(0))[2];
+								if( t.add != null ) {
+									for( int j = 0; j<t.add.size(); j++ ) {
+										String[] s = t.add.get(j);
+										tStart = Math.min(tStart, Integer.parseInt(s[3]) );
+										tEnd = Math.max(tEnd, Integer.parseInt(s[4]));
+									}
 								}
 								
+								w.append( g.id + "\t" + c + "\t" + tStart + "\t" + tEnd + "\t"+ g.strand + "\t" + e.getKey() + "\t" + parts.length() + "\t" );
 								
-								//tie
-								if( donSites != null ) {
-									int v = g.strand==1 ? part[1] : (part[2]+1);
+								double tie=0;
+								int last=-10;
+								int covered=0, l=0, idx;
+								int[][] donSites = sites==null? null : sites[g.strand==1?0:1];
+								nuc.delete( 0, nuc.length() );
+								double sum=0;
+								int min=Integer.MAX_VALUE;
+								for( int j = 0;j < parts.length(); j++ ) {
+									int[] part = g.exon.get(parts.get(j));
 									
-									idx = Arrays.binarySearch( donSites[0], last );
-									if( idx > 0 ) {
-										while( idx>0 &&  donSites[0][idx-1] == last ) {
-											idx--;
-										}
-									}
-									if( idx >= 0 ) {
-										while( idx < donSites[0].length && donSites[0][idx] == last && donSites[1][idx] != v ) {
-											idx++;
-										}
-										if( idx < donSites[0].length && donSites[0][idx] == last && donSites[1][idx] == v ) {
-											tie++;
-										}
-									}
-								}
-								
-								//cov
-								int start = part[1];//t.targetStart;
-								int end = part[2];//t.targetEnd;
-								l += end-start+1;
-								if( cov != null ) {
-									idx = Arrays.binarySearch(cov, new int[]{start}, GeMoMa.IntArrayComparator.comparator[2] );
-									if( idx < 0 ) {
-										idx = -(idx+1);
-										idx = Math.max(0, idx-1);
+									if( g.strand> 0 ) {
+										nuc.append( seq.substring( part[1]-1, part[2] ) );
+									} else {
+										nuc.insert(0, seq.substring( part[1]-1, part[2] ) );
 									}
 									
-									int[] inter = cov[idx];
-									//System.out.println("hier " + start + " .. " + end + "\t" + Arrays.toString(inter) );
-									int p = start;
-									outerloop: while( p <= end ) {
-										while( p > inter[1] ) {
-											idx++;
-											if( idx < cov.length ) {
-												inter = cov[idx];
-											} else {
-												min=0;
-												break outerloop;
+									
+									//tie
+									if( donSites != null ) {
+										int v = g.strand==1 ? part[1] : (part[2]+1);
+										
+										idx = Arrays.binarySearch( donSites[0], last );
+										if( idx > 0 ) {
+											while( idx>0 &&  donSites[0][idx-1] == last ) {
+												idx--;
 											}
 										}
-										if( inter[0]<= p && p <=inter[1] ) {
-											int h = Math.min(inter[1],end)+1;
-											int z=h-p;
-											covered+=z;
-											
-											sum+=inter[2] * z;
-											min = Math.min(min, inter[2]);
-											
-											p=h;
-										} else {//p<inter[0] && p<=inter[1]
-											min=0;
-											p = Math.min(inter[0],end+1);
+										if( idx >= 0 ) {
+											while( idx < donSites[0].length && donSites[0][idx] == last && donSites[1][idx] != v ) {
+												idx++;
+											}
+											if( idx < donSites[0].length && donSites[0][idx] == last && donSites[1][idx] == v ) {
+												tie++;
+											}
 										}
-										//System.out.println(p + "\t" + Arrays.toString(inter) + "\t" + covered + "\t" + min);
 									}
+									
+									//cov
+									int start = part[1];//t.targetStart;
+									int end = part[2];//t.targetEnd;
+									l += end-start+1;
+									if( cov != null ) {
+										idx = Arrays.binarySearch(cov, new int[]{start}, GeMoMa.IntArrayComparator.comparator[2] );
+										if( idx < 0 ) {
+											idx = -(idx+1);
+											idx = Math.max(0, idx-1);
+										}
+										
+										int[] inter = cov[idx];
+										//System.out.println("hier " + start + " .. " + end + "\t" + Arrays.toString(inter) );
+										int p = start;
+										outerloop: while( p <= end ) {
+											while( p > inter[1] ) {
+												idx++;
+												if( idx < cov.length ) {
+													inter = cov[idx];
+												} else {
+													min=0;
+													break outerloop;
+												}
+											}
+											if( inter[0]<= p && p <=inter[1] ) {
+												int h = Math.min(inter[1],end)+1;
+												int z=h-p;
+												covered+=z;
+												
+												sum+=inter[2] * z;
+												min = Math.min(min, inter[2]);
+												
+												p=h;
+											} else {//p<inter[0] && p<=inter[1]
+												min=0;
+												p = Math.min(inter[0],end+1);
+											}
+											//System.out.println(p + "\t" + Arrays.toString(inter) + "\t" + covered + "\t" + min);
+										}
+									}
+									
+									last = g.strand==1 ? part[2]+1 : part[1];
 								}
 								
-								last = g.strand==1 ? part[2]+1 : part[1];
-							}
-							
-							//check
-							String cod;
-							if( g.strand < 0 ) {
-								cod = Tools.rc(nuc.toString());
-							} else {
-								cod = nuc.toString();
-							}
-							String aa = Tools.translate(0, cod, code, false, Ambiguity.AMBIGUOUS);
-							
-							int preMatureStops=0;
-							for( int j=0; j < aa.length()-1; j++ ) {
-								if( aa.charAt(j)=='*') preMatureStops++;
-							}
-							
-							String tieString = ( parts.length() == 1 ) ? "NA" : GeMoMa.decFormat.format( tie/(parts.length()-1d));
-							String tpcString = ( GeMoMa.coverage == null ) ? "NA" : GeMoMa.decFormat.format(covered/(double)l);
-							String avgCovString = ( GeMoMa.coverage == null ) ? "NA" : GeMoMa.decFormat.format(sum/(double)l);
-							String minCovString = ( GeMoMa.coverage == null ) ? "NA" : (""+min);
-							
-							if( (tie==parts.length()-1) && covered==l ) {
-								perfect++;
-							}
-							
-							w.append( tieString + "\t" + tpcString + "\t" + minCovString + "\t" + avgCovString + "\t" + preMatureStops );
-							w.newLine();
-							
-							//annotation
-							annot.append( c + "\t" + g.evidence + "+AnnotationEvidence\t"+tag+"\t" + tStart + "\t" + tEnd + "\t.\t" + (g.strand==1?"+":"-") + "\t.\t");
-							//attributes
-							String[] att = t.attributes==null ? EMPTY : t.attributes.split(";");
-							for( int j = 0; j < att.length; j++ ) {
-								att[j]=att[j].trim();
-							}
-							boolean gtf = att.length>=2 && (att[0].startsWith("gene_id") || att[1].startsWith("transcript_id"));
-							if( gtf ) {
-								//GTF -> GFF
-								for( int j = 0; j < att.length; j++ ) {
-									att[j]=att[j].replace(" \"", "=\"");
-								}
-								if( att[0].startsWith("gene_id") ) {
-									att[0]=att[0].replace("gene_id", "Parent" );
-									att[1]=att[1].replace("transcript_id", "ID" );
+								//check
+								String cod;
+								if( g.strand < 0 ) {
+									cod = Tools.rc(nuc.toString());
 								} else {
-									att[1]=att[1].replace("gene_id", "Parent" );
-									att[0]=att[0].replace("transcript_id", "ID" );
+									cod = nuc.toString();
 								}
-							}
-							attr.clear();
-							for( int j = 0; j < att.length; j++ ) {
-								int pos = att[j].indexOf('=');
-								attr.put(att[j].substring(0,pos), att[j].substring(pos+1));
-							}
-							
-							old.delete(0, old.length());
-							
-							add(old,attr,"ID",e.getKey());
-							add(old,attr,"Parent",g.id);
-							add(old,attr,"aa",""+(l/3));
-							if( introns ) add(old,attr,"tie",tieString);
-							if( coverage ) {
-								add(old,attr,"tpc",tpcString);
-								add(old,attr,"minCov",""+min);
-								add(old,attr,"avgCov",avgCovString);
-							}
-							add(old,attr,"nps",""+preMatureStops);
-							add(old,attr,"start",""+aa.charAt(0));
-							add(old,attr,"stop",""+aa.charAt(aa.length()-1));
-							add(old,attr,"ce",""+parts.length());
-							
-							//write attributes in the same order as in the input file
-							for( int j = 0; j < att.length; j++ ) {
-								int pos = att[j].indexOf('=');
-								String key = att[j].substring(0,pos);
-								annot.append(key+"=" + attr.remove(key) + ";");
-							}
-							//add additional attributes
-							String[] k = attr.keySet().toArray(EMPTY);
-							Arrays.sort(k);
-							for( String key: k ) {
-								annot.append(key+"=" + attr.remove(key) + ";");
-							}
-							if( old.length()>0 ) annot.append("#old: " + old);
-							
-							/*
-							annot.append( "ID=" + e.getKey() + ";Parent="+g.id + ";"
-										+ "aa=" + (l/3) 
-										+ (introns?";tie=" + tieString:"") 
-										+ (coverage?(";tpc="+ tpcString + ";minCov=" + min + ";avgCov=" + avgCovString):"")
-										+ ";nps=" + preMatureStops										
-										+ ";start=" + aa.charAt(0) + ";stop=" + aa.charAt(aa.length()-1) );
-							*/
-							annot.newLine();
-							if( t.add != null ) {
-								for( int j = 0; j < t.add.size(); j++ ) {
-									String[] split = t.add.get(j);
-									for( int m = 0; m < split.length; m++ ) {
-										annot.append( (m==0?"":"\t") + split[m] );
+								String aa = Tools.translate(0, cod, code, false, Ambiguity.AMBIGUOUS);
+								
+								int preMatureStops=0;
+								for( int j=0; j < aa.length()-1; j++ ) {
+									if( aa.charAt(j)=='*') preMatureStops++;
+								}
+								
+								String tieString = ( parts.length() == 1 ) ? "NA" : GeMoMa.decFormat.format( tie/(parts.length()-1d));
+								String tpcString = ( GeMoMa.coverage == null ) ? "NA" : GeMoMa.decFormat.format(covered/(double)l);
+								String avgCovString = ( GeMoMa.coverage == null ) ? "NA" : GeMoMa.decFormat.format(sum/(double)l);
+								String minCovString = ( GeMoMa.coverage == null ) ? "NA" : (""+min);
+								
+								if( (tie==parts.length()-1) && covered==l ) {
+									perfect++;
+								}
+								
+								w.append( tieString + "\t" + tpcString + "\t" + minCovString + "\t" + avgCovString + "\t" + preMatureStops );
+								w.newLine();
+								
+								//annotation
+								annot.append( c + "\t" + g.evidence + "+AnnotationEvidence\t"+tag+"\t" + tStart + "\t" + tEnd + "\t.\t" + (g.strand==1?"+":"-") + "\t.\t");
+								//attributes
+								String[] att = t.attributes==null ? EMPTY : t.attributes.split(";");
+								for( int j = 0; j < att.length; j++ ) {
+									att[j]=att[j].trim();
+								}
+								boolean gtf = att.length>=2 && (att[0].startsWith("gene_id") || att[1].startsWith("transcript_id"));
+								if( gtf ) {
+									//GTF -> GFF
+									for( int j = 0; j < att.length; j++ ) {
+										att[j]=att[j].replace(" \"", "=\"");
 									}
+									if( att[0].startsWith("gene_id") ) {
+										att[0]=att[0].replace("gene_id", "Parent" );
+										att[1]=att[1].replace("transcript_id", "ID" );
+									} else {
+										att[1]=att[1].replace("gene_id", "Parent" );
+										att[0]=att[0].replace("transcript_id", "ID" );
+									}
+								}
+								attr.clear();
+								for( int j = 0; j < att.length; j++ ) {
+									int pos = att[j].indexOf('=');
+									attr.put(att[j].substring(0,pos), att[j].substring(pos+1));
+								}
+								
+								old.delete(0, old.length());
+								
+								add(old,attr,"ID",e.getKey());
+								add(old,attr,"Parent",g.id);
+								add(old,attr,"aa",""+(l/3));
+								if( introns ) add(old,attr,"tie",tieString);
+								if( coverage ) {
+									add(old,attr,"tpc",tpcString);
+									add(old,attr,"minCov",""+min);
+									add(old,attr,"avgCov",avgCovString);
+								}
+								add(old,attr,"nps",""+preMatureStops);
+								add(old,attr,"start",""+aa.charAt(0));
+								add(old,attr,"stop",""+aa.charAt(aa.length()-1));
+								add(old,attr,"ce",""+parts.length());
+								
+								//write attributes in the same order as in the input file
+								for( int j = 0; j < att.length; j++ ) {
+									int pos = att[j].indexOf('=');
+									String key = att[j].substring(0,pos);
+									annot.append(key+"=" + attr.remove(key) + ";");
+								}
+								//add additional attributes
+								String[] k = attr.keySet().toArray(EMPTY);
+								Arrays.sort(k);
+								for( String key: k ) {
+									annot.append(key+"=" + attr.remove(key) + ";");
+								}
+								if( old.length()>0 ) annot.append("#old: " + old);
+								
+								/*
+								annot.append( "ID=" + e.getKey() + ";Parent="+g.id + ";"
+											+ "aa=" + (l/3) 
+											+ (introns?";tie=" + tieString:"") 
+											+ (coverage?(";tpc="+ tpcString + ";minCov=" + min + ";avgCov=" + avgCovString):"")
+											+ ";nps=" + preMatureStops										
+											+ ";start=" + aa.charAt(0) + ";stop=" + aa.charAt(aa.length()-1) );
+								*/
+								annot.newLine();
+								if( t.add != null ) {
+									for( int j = 0; j < t.add.size(); j++ ) {
+										String[] split = t.add.get(j);
+										for( int m = 0; m < split.length; m++ ) {
+											annot.append( (m==0?"":"\t") + split[m] );
+										}
+										annot.newLine();
+									}
+								}
+								for( int j = 0;j < parts.length(); j++ ) {
+									int[] part = g.exon.get(parts.get(j));
+									annot.append( c + "\t" + g.evidence + "\tCDS\t" + part[1] + "\t" + part[2] + "\t.\t" + (g.strand==1?"+":"-") + "\t"+(part[3]==Part.NO_PHASE?".":part[3])+"\tParent=" + e.getKey() );
 									annot.newLine();
 								}
-							}
-							for( int j = 0;j < parts.length(); j++ ) {
-								int[] part = g.exon.get(parts.get(j));
-								annot.append( c + "\t" + g.evidence + "\tCDS\t" + part[1] + "\t" + part[2] + "\t.\t" + (g.strand==1?"+":"-") + "\t"+(part[3]==Part.NO_PHASE?".":part[3])+"\tParent=" + e.getKey() );
-								annot.newLine();
 							}
 						}
 					}
