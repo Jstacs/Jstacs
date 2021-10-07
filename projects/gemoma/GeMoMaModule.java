543844ae119eb974ccedcbb18a7a14502c9779cf
Jens Keilwagen <Jens.Keilwagen@googlemail.com>
2021-10-07 08:30:38 +0200

    1.8

--- projects/gemoma/GeMoMaModule.java
+++ projects/gemoma/GeMoMaModule.java
@@ -34,7 +34,7 @@ import de.jstacs.tools.ToolResult;
  */
 public abstract class GeMoMaModule implements JstacsTool {
 
-	public static final String VERSION = "1.7.1";
+	public static final String VERSION = "1.8";
 	
 	public static final String INFO = "#SOFTWARE INFO: ";
 
