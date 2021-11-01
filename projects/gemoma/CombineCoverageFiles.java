6bba4ee7d33eff4f3e594c64fbc2597b89814fe6
Jens Keilwagen <Jens.Keilwagen@googlemail.com>
2021-11-01 10:19:23 +0100

    make it more memory efficient

--- projects/gemoma/CombineCoverageFiles.java
+++ projects/gemoma/CombineCoverageFiles.java
@@ -24,7 +24,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * A simple tool for combining coverage files that might be created in parallel using a compute cluster.
@@ -33,84 +32,145 @@ import java.util.HashMap;
  */
 public class CombineCoverageFiles {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, CloneNotSupportedException {
 		System.out.println("files: " + (args.length-1));
 		
 		BufferedWriter w = new BufferedWriter( new FileWriter(args[0]) );
 		//read files
-		BufferedReader r;
-		HashMap<String,HashMap<Integer,int[]>> combined = new HashMap<String,HashMap<Integer,int[]>>();
-		HashMap<Integer,int[]> current;
-		
+		BufferedReader[] r = new BufferedReader[args.length-1];
+		BedEntry[] bed = new BedEntry[args.length-1];
 		String line;
-		for( int i = 1; i < args.length; i++ ) {
-			System.out.println((i-1) + "\t" + args[i]);
-			r = new BufferedReader( new FileReader( args[i] ) );
+		for( int i = 0; i < r.length; i++ ) {
+			System.out.println(i + "\t" + args[i+1]);
+			r[i] = new BufferedReader( new FileReader( args[i+1] ) );
 			//skip header
-			line=r.readLine();
-			if( i==1 ) {
+			line=r[i].readLine();
+			if( i==0 ) {
 				w.append(line);
 				w.newLine();
 			}
-			
-			//add to hash
-			String old=null;
-			current = null;
-			while( (line=r.readLine()) != null ) {
-				String[] split = line.split("\t");
-				if( !split[0].equals(old) ) {
-					old=split[0];
-					current = combined.get(split[0]);
-					if( current == null ) {
-						current = new HashMap<Integer,int[]>();
-						combined.put(split[0], current);
-					}
-				}
-				int st = Integer.parseInt(split[1]);
-				int en = Integer.parseInt(split[2]);
-				int cov = Integer.parseInt(split[3]);
-				for( int j = st; j < en; j++ ) {
-					int[] a = current.get(j);
-					if( a==null ) {
-						a=new int[1];
-						current.put(j, a);
-					}
-					a[0] += cov;
-				}
-			}
-			r.close();
+			bed[i]=new BedEntry(r[i].readLine()); 
 		}
-
-		//write
-		System.out.println();
-		System.out.println("write");
-		String[] chr = combined.keySet().toArray(new String[0]);
-		Arrays.sort(chr);
-		for( int i = 0; i < chr.length; i++ ) {
-			current = combined.get(chr[i]);
-			Integer[] keys = current.keySet().toArray(new Integer[0]);
-			Arrays.sort(keys);
-			int start=-1, last=-1, cov=-1;
-			for( int j = 0; j<keys.length; j++ ) {
-				int[] c = current.get(keys[j]);
-				//System.out.println(keys[j] + "\t" + c[0] );
-				if( last+1==keys[j] && cov == c[0] ) {
-					//proceed
-					last++;
-				} else {
-					if( start> 0 ) {
-						w.write( chr[i] + "\t" + start + "\t" + (last+1) + "\t" + cov );
-						w.newLine();
+		
+		BedEntry current = null, next;
+		boolean[] sameStart = new boolean[r.length];
+		int same=0;
+		do {
+			//find next
+			next=null;
+			same=0;
+			Arrays.fill(sameStart, false);
+			for( int i = 0; i < bed.length; i++ ) {
+				if( bed[i]!=null ) {
+					if( next==null ) {
+						next=bed[i];
+						sameStart[i]=true;
+						same++;
+					} else {
+						int comp=next.compareTo(bed[i]);
+						if( comp>=0 ) {
+							if( comp>0 ) {
+								Arrays.fill(sameStart, false);
+								next=bed[i];
+								same=0;
+							}
+							sameStart[i]=true;
+							same++;
+						}
 					}
-					start=last=keys[j];
-					cov=c[0];
 				}
 			}
-			if( start> 0 ) {
-				w.write( chr[i] + "\t" + start + "\t" + (last+1) + "\t" + cov );
-				w.newLine();
+			
+			if( same>0 ) {
+				//sum
+				BedEntry now = next.clone();
+				boolean first=true;
+				for( int i = 0; i < bed.length; i++ ) {
+					if( sameStart[i] ) {
+						if( !first ) {
+							if( bed[i].end < now.end ) {
+								now.end = bed[i].end;
+							}
+							now.cov += bed[i].cov;
+						} else {
+							first=false;
+						}
+					} else {
+						if( bed[i] != null ) {
+							if( bed[i].chr.equals(now.chr) && bed[i].start<now.end ) {
+								now.end=bed[i].start;
+							}
+						}
+					}
+				}
+				
+				
+				//update array
+				for( int i = 0; i < sameStart.length; i++ ) {
+					if( sameStart[i] ) {
+						if( bed[i].end == now.end ) {
+							line = r[i].readLine();
+							if( line != null ) {
+								bed[i] = new BedEntry(line);
+							} else {
+								bed[i] = null;
+							}
+						} else {
+							//shorten
+							bed[i].start=now.end;
+						}
+					}
+				}
+				
+				//write
+				if( current != null ) {
+					if( current.chr.equals(now.chr) && current.end == now.start && current.cov==now.cov ) {
+						//extend
+						current.end = now.end;
+					} else {
+						//write
+						current.write(w);
+						current=now;
+					}
+				} else {
+					current=now;
+				}
 			}
+		} while( same>0 );
+		if( current !=null ) {
+			current.write(w);
 		}
 		w.close();
+	}
+
+	static class BedEntry implements Comparable<BedEntry>, Cloneable {
+		String chr;
+		int start, end, cov;
+		
+		BedEntry( String line ) {
+			String[] split = line.split("\t");
+			chr=split[0];
+			start = Integer.parseInt(split[1]);
+			end = Integer.parseInt(split[2]);
+			cov = Integer.parseInt(split[3]);
+		}
+		
+		public BedEntry clone() throws CloneNotSupportedException {
+			return (BedEntry) super.clone();
+		}
+
+		@Override
+		public int compareTo(BedEntry o) {
+			int diff = chr.compareTo(o.chr);
+			if( diff == 0 ) {
+				diff=Integer.compare(start, o.start);
+			}
+			return diff;
+		}
+		
+		public void write( BufferedWriter w ) throws IOException {
+			w.write(chr + "\t" + start + "\t" + end + "\t" + cov );
+			w.newLine();
+		}
 	}
 }
