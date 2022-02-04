85f070b46c392eda90c4cbef124bd4dda5f45264
Jens Keilwagen <Jens.Keilwagen@googlemail.com>
2022-02-04 07:27:44 +0100

    improved regex for fasta sequence IDs

--- projects/gemoma/Tools.java
+++ projects/gemoma/Tools.java
@@ -607,7 +607,7 @@ public class Tools {
 	}
 	
 	public static HashMap<String,String> getFasta( String fName, int initSize ) throws Exception {
-		return getFasta(fName, initSize, "([a-zA-Z\\-\\.:0-9]+(_\\d+)?)|([a-zA-Z\\-_\\.:0-9]+_\\d+)");
+		return getFasta(fName, initSize, "([^_]+)|(.+_\\d+)");
 	}
 	
 	public static HashMap<String,String> getFasta( String fName, int initSize, String seqIdRegex ) throws Exception {
