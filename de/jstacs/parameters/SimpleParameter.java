1e023023eb9cfb5eddb0e12c3d074b8be82607e6
Jens Keilwagen <Jens.Keilwagen@googlemail.com>
2022-03-28 15:32:46 +0200

    improved checkValue method

--- de/jstacs/parameters/SimpleParameter.java
+++ de/jstacs/parameters/SimpleParameter.java
@@ -266,61 +266,51 @@ public class SimpleParameter extends Parameter implements GalaxyConvertible {
 		if( value == null ) {
 			return true;
 		}
-		if (validator == null) {
-			boolean res;
-			try {
-				switch ( datatype ) {
-					case BOOLEAN: res = value instanceof Boolean || value instanceof String; break;
-					case CHAR: res = value instanceof Character	|| (value instanceof String && ((String) value).length() == 1); break;
-					case BYTE: res = value instanceof Byte || (value instanceof String && new Byte((String) value) != null ); break;
-					case SHORT: res= value instanceof Short || (value instanceof String && new Short((String) value) != null ); break;
-					case INT: res = value instanceof Integer || (value instanceof String && new Integer((String) value) != null ); break;
-					case LONG: res = value instanceof Long || (value instanceof String && new Long((String) value) != null ); break;
-					case FLOAT: res = value instanceof Float || (value instanceof String && new Float((String) value) != null );break;
-					case DOUBLE: res = value instanceof Double || (value instanceof String && new Double((String) value) != null ); break;
-					case STRING: res = value instanceof String; break;
-					default: res = false;
-				}
-			} catch ( Exception e ) {
-				res = false;
+		boolean res;
+		try {
+			switch ( datatype ) {
+				case BOOLEAN: res = value instanceof Boolean || value instanceof String; break;
+				case CHAR: res = value instanceof Character	|| (value instanceof String && ((String) value).length() == 1); break;
+				case BYTE: res = value instanceof Byte || (value instanceof String && new Byte((String) value) != null ); break;
+				case SHORT: res= value instanceof Short || (value instanceof String && new Short((String) value) != null ); break;
+				case INT: res = value instanceof Integer || (value instanceof String && new Integer((String) value) != null ); break;
+				case LONG: res = value instanceof Long || (value instanceof String && new Long((String) value) != null ); break;
+				case FLOAT: res = value instanceof Float || (value instanceof String && new Float((String) value) != null );break;
+				case DOUBLE: res = value instanceof Double || (value instanceof String && new Double((String) value) != null ); break;
+				case STRING: res = value instanceof String; break;
+				default: res = false;
 			}
-			if( res  ) {
-				errorMessage = "";
-			} else {
-				errorMessage = "The specified value is no " + datatype + ".";
-			}
-			return res;
-		} else {
+		} catch ( Exception e ) {
+			res = false;
+		}
+		if( res  ) {
 			Object value2 = value;
 			if (value instanceof String) {
 				//if (((String) value).length() > 0) {
 					try {
 						switch( datatype ) {
+							case BOOLEAN: value2 = new Boolean((String) value); break;
 							case BYTE: value2 = new Byte((String) value); break;
+							case CHAR: value2 = ((String) value).charAt(0); break;
 							case SHORT: value2 = new Short((String) value); break;
 							case INT: value2 = new Integer((String) value); break;
 							case LONG: value2 = new Long((String) value); break;
 							case FLOAT: value2 = new Float((String) value); break;
 							case DOUBLE: value2 = new Double((String) value); break;
+							//other datatypes are not allowed for this class
 						}
 					} catch (NumberFormatException e) {
 						errorMessage = "Value is not of the expected format.";
 						return false;
 					}
-				//} else {
-				//	errorMessage = "";
-				//	return false;
-				//}
 			}
 
-			if (validator.checkValue(value2)) {
-				errorMessage = validator.getErrorMessage();
-				return true;
-			} else {
-				errorMessage = validator.getErrorMessage();
-				return false;
-			}
+			res = validator.checkValue(value2);
+			errorMessage = validator.getErrorMessage();
+		} else {
+			errorMessage = "The specified value is no " + datatype + ".";
 		}
+		return res;
 	}
 
 	/*
@@ -388,7 +378,7 @@ public class SimpleParameter extends Parameter implements GalaxyConvertible {
 						value = new Boolean(s);
 						break;
 					case CHAR:
-						value = s.toCharArray()[0];
+						value = s.charAt(0);
 						break;
 					case BYTE:
 						value = new Byte(s);
