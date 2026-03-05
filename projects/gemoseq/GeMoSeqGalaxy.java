8d34cc76187b93a1b2bd2169b7d9a32d4dcf12d2
Jens Keilwagen <Jens.Keilwagen@googlemail.com>
2026-03-05 15:13:13 +0100

    include GAF

--- projects/gemoseq/GeMoSeqGalaxy.java
+++ projects/gemoseq/GeMoSeqGalaxy.java
@@ -1,12 +1,13 @@
 package projects.gemoseq;
 
 import de.jstacs.tools.ui.galaxy.Galaxy;
+import projects.gemoma.GeMoMaAnnotationFilter;
 
 public class GeMoSeqGalaxy {
 
 	public static void main(String[] args) throws Exception {
 		
-		Galaxy galaxy = new Galaxy("", true, true, new TranscriptPrediction(), new MergeGeMoMaGeMoSeq());
+		Galaxy galaxy = new Galaxy("", true, true, new TranscriptPrediction(), new MergeGeMoMaGeMoSeq(), new GeMoMaAnnotationFilter());
 		
 		galaxy.run(args);
 
